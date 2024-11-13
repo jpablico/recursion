241874732924a904dee33b1f427f7b4efaf64dae
Josh Pablico <93159092+jpablico@users.noreply.github.com>
2024-11-12 20:33:15 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import IgnoreEmitPlugin from 'ignore-emit-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const publicPath = isProduction ? '/<REPLACE THIS WITH REPO>/' : '/';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  entry: ['./src/scripts/index.js', './src/styles/style.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: publicPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets',
+              publicPath: 'assets',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new IgnoreEmitPlugin(['style.js']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+  },
+};
